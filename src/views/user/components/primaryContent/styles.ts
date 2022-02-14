c016f5e6e88c5c969180de31754ab6af92bc6668
joao.chagas <joao.chagas@webjump.com.br>
2022-02-14 15:04:00 -0300

    update and adjust routes changes manage, beyond add user module for login and register

---
+++ src/views/user/components/primaryContent/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ContentStyled = styled("div")`
+  min-width: 65%;
+  height: 100%;
+  background-color: #ffffff;
+  margin: auto;
+`;
--- src/views/user/styles.ts
+++ src/views/user/styles.ts
@@ -1,7 +1,68 @@
-import styled from "styled-components";
+import styled from '@emotion/styled'
+import { css } from '@mui/styled-engine'
 
-export const Styles: any = styled.div`
+export const RowDivStyled = styled('div')`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
   height: 100%;
+  min-width: 1200px;
+`
+
+export const ImageLogo = styled('img')`
+  margin: 0 auto;
+`
+
+export const BaseStyled = styled('div')`
+  ${({ justifyContent }: { justifyContent: 'space-around' | 'space-evenly' }) =>
+    css`
+      margin: auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: ${justifyContent};
+      width: 100%;
+      height: 100%;
+    `}
+`
+
+export const CenterVerticalContentStyled = styled('div')`
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
-`;
+  row-gap: 64px;
+  align-items: center;
+  text-align: center;
+  margin: 0 40px;
+`
+
+export const Title = styled('div')`
+  ${({ color, fontWeight }: { color: string; fontWeight: number }) =>
+    css`
+      font-size: 64px;
+      color: ${color};
+      font-weight: ${fontWeight};
+    `}
+`
+
+export const SubTitle = styled('div')`
+  font-size: 30px;
+  font-weight: 300;
+  color: white;
+`
+
+export const BottomButton = styled('button')`
+  width: 360px;
+  height: 72px;
+  border: 3px solid #ffffff;
+  border-radius: 45px;
+  background-color: #e83f5b;
+  font-size: 24px;
+  font-weight: 300;
+  color: #ffffff;
+  text-align: center;
+  margin: 0 auto;
+  :hover {
+    cursor: pointer;
+    background-color: #9c283c;
+  }
+`
